2d080e27fbc035a1a88dee9a73eef66dfa386ce5
NemoKingFish <anakornduangjai@gmail.com>
2025-05-07 20:08:25 +0700

    Credit

--- Assets/Script/02/Bullet.cs
+++ Assets/Script/02/Bullet.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 10f;
+    public float delay = 3f;
 
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
 
-    
+    void Start()
+    {
+        Destroy(gameObject, delay);
+    }
+
 }
 
---
+++ Assets/Script/CreditManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditManager : MonoBehaviour
+{
+    [SerializeField] private GameObject creditPanel; //อิงCreditPanel
+    [SerializeField] private GameObject uiPanel; //UIหลักที่ต้องซ่อน
+
+    void Start()
+    {
+        creditPanel.SetActive(false); // ซ่อนเครดิต
+    }
+
+    public void ShowCredits()
+    {
+        creditPanel.SetActive(true);  //เปิกเครดิต
+        uiPanel.SetActive(false); //ซ่อน UI 
+        Time.timeScale = 0;
+    }
+
+    public void HideCredits()
+    {
+        creditPanel.SetActive(false); 
+        uiPanel.SetActive(true);
+        Time.timeScale = 1;
+    }
+}
